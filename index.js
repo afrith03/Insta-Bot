6f9ed2e4748dc716ee1cccab12e6fcd3499b35d6
afrith03 <afrithshariff20@gmail.com>
2023-02-02 11:22:22 +0530

    dark and white theme

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 const Quotes = require("randomquote-api");
 
 let caption =
-  "Your views about this Post..?\n.\nThe comment section is yours!!\n.\nI post QUOTES daily\n.\n.\n.\nfollow\n@quotesbymyth\n@quotesbymyth\n@quotesbymyth\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n#quotesbymyth #myth'squotes #wordsofwisdom #entrepreneur #goals #follow #quotesdaily #loveyourself #business #instadaily #wordporn #like #writersofig #happy #instawriters #writersofindia #yourquote #stories #art #quotesaboutlife #igwriters #writeaway #wordswag #igwritersclub #photography #motivate #nevergiveup #hustle #selfcare #yourself";
+  "Your views about this Post..?\n.\nThe comment section is yours!!\n.\nI post QUOTES daily\n.\n.\n.\nfollow\n@quotesbymyth\n@quotesbymyth\n@quotesbymyth\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n#quotesbymyth #wordsofwisdom #entrepreneur #goals #follow #quotesdaily #loveyourself #business #instadaily #wordporn #like #writersofig #happy #instawriters #writersofindia #yourquote #stories #art #quotesaboutlife #igwriters #writeaway #wordswag #igwritersclub #photography #motivate #nevergiveup #hustle #selfcare #yourself";
 
 const postToInsta = async () => {
   try {
@@ -23,9 +23,12 @@ const postToInsta = async () => {
 
     const randomquote = Quotes.randomQuote();
     let quoteToInsta = randomquote.quote.split(" ").join("%20");
+    let theme = Math.round(Math.random() * 10) > 5;
     //  https://unsplash.it/200/300/?random
     const imageBuffer = await get({
-      url: `https://web-series-quotes-api.deta.dev/pic/custom?text=${quoteToInsta}&background_color=white&text_color=black&text_size=150&x=1080&y=1080`,
+      url: `https://web-series-quotes-api.deta.dev/pic/custom?text=${quoteToInsta}&background_color=${
+        theme ? "white" : "black"
+      }&text_color=${!theme ? "white" : "black"}&text_size=200&x=3600&y=3600`,
       encoding: null,
     });
 
@@ -33,6 +36,7 @@ const postToInsta = async () => {
       file: imageBuffer,
       caption: caption,
     });
+    console.log(quoteToInsta);
     return quoteToInsta;
     // console.log(typeof(process.env.UNAME))
   } catch (error) {
@@ -51,7 +55,7 @@ app.post("/twitter", async (req, res) => {
 });
 
 // const cronInsta = new CronJob("30 * * * * *", async () => {
-//     postToInsta();
+postToInsta();
 // });
 
 // cronInsta.start();
